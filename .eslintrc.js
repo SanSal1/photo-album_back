b3ce7f85eac57158238f1018ac46114d55a5e39d
Sanna Salonen <sanna.m.salonen@tuni.fi>
2023-09-05 13:01:21 +0300

    Setup Eslint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    eqeqeq: 'error',
+    'no-console': 'off',
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    'linebreak-style': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  ignorePatterns: ['node_modules', 'build', 'dist', 'public'],
+};
--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (_req, res) => {
-  res.send("Home");
+app.get('/', (_req, res) => {
+  res.send('Home');
 });
 
 app.listen(port, () => {
